afce26b504b27d44af2cf0f8e331a0fc075bb1a7
MaximMatusevich <imulir@gmail.com>
2022-05-21 23:17:11 +0400

    init

---
+++ MMDesignSystem/MMDesignSystem.h
@@ -0,0 +1,18 @@
+//
+//  MMDesignSystem.h
+//  MMDesignSystem
+//
+//  Created by Максим Матусевич on 21.05.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MMDesignSystem.
+FOUNDATION_EXPORT double MMDesignSystemVersionNumber;
+
+//! Project version string for MMDesignSystem.
+FOUNDATION_EXPORT const unsigned char MMDesignSystemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMDesignSystem/PublicHeader.h>
+
+
